4d97cc9565ae3849e27266c8cb61344a628e1cf7
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2019-03-09 19:24:48 +0530

    Update Data_structure_qn2.cpp

--- Data_structure_qn2.cpp
+++ Data_structure_qn2.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include<stdio.h>
-#include <stdlib.h>
-
+#include <stdlib.h>								//header_files
+using namespace std
 int main()
 {   
-	int tube[100]={0};
+	int tube[100]={0};							//initialisation of variables
 	int a=1,choice,i=-1;
 	while(a)
 	{   
-		std::cout<<"\n1.Add\n2.Remove\n3.Exit";
-		std::cout<<"\nEnter the opton :";
-		std::cin>>choice;
-		switch(choice)
+		cout<<"\n1.Add\n2.Remove\n3.Exit";
+		cout<<"\nEnter the option :";
+		cin>>choice;
+		switch(choice)							//getting choices
 		{
-			case 1: if(i==100)
-				    std::cout<<"\nTube is full.";
+			case 1: if(i==100)					//adding
+				    cout<<"\nTube is full.";	
 				    else
 				    {tube[++i]=1;
-				     std::cout<<"\nOne person is added.";
+				     cout<<"\n one person is added.";
 				    }
-                                    std::cout<<"\nNo. of people inside = "<<i+1;
+                                     cout<<"\nNo. of people inside = "<<i+1;
 				    break;
-			case 2: if(i<0)
+			case 2: if(i<0)						//removing
 				    std::cout<<"\nThe Tube is empty.";
 				    else
 				    {tube[i--]=0;
