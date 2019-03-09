4b97b282413e36640cf7353d62c6d78077c6d590
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2019-03-09 15:59:10 +0530

    Update and rename ROS_qn3 to ROS_qn3.py

--- ROS_qn3.py
+++ ROS_qn3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import rospy                                   //for ROS packages
+import rospy                                   #for ROS packages
 from std_msgs.msg import String
 def callback(msg):                            
    print msg.data
-rospy.init_node('topic_subscriber')            //initialisation for subscription
+rospy.init_node('topic_subscriber')            #initialisation for subscription
 sub = rospy.Subscriber('random_messages', String, callback)  //Telling the channel to master node
 rospy.spin()
